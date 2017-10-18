0cbfc8e0503143ad7e41cdb96b50fa82307af4c0
Gardien Benjamin <benjamin.gardien@viacesi.fr>
2017-10-18 11:29:22 +0200

    :wrench: add MacOS support

--- src/main/java/pimpmyfridge/controller/SerialManager.java
+++ src/main/java/pimpmyfridge/controller/SerialManager.java
@@ -18,7 +18,7 @@ public class SerialManager implements SerialPortEventListener {
     JSONObject sensor;
     /** The port we're normally going to use. */
     private static final String PORT_NAMES[] = {
-            "/dev/tty.usbserial-A9007UX1", // Mac OS X
+            "/dev/cu.usbmodem1411", // Mac OS X
             "/dev/ttyACM0", // Raspberry Pi
             "/dev/ttyUSB0", // Linux
             "COM3", // Windows
@@ -37,7 +37,7 @@ public class SerialManager implements SerialPortEventListener {
     public void initialize() {
         // the next line is for Raspberry Pi and
         // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
-        System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+        // System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
 
         CommPortIdentifier portId = null;
         Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
